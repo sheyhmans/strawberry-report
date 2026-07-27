0cc280017462a6f88403ec72c6b493d7d8fa9efa
sheyhmans <sheyhmans3585@gmail.com>
2026-07-28 02:32:57 +0300

    Implement product addition functionality
    Added functionality to dynamically add product elements with fields for name, quantity, and price.

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+let productCount = 1;
 
+function addProduct() {
+
+productCount++;
+
+let client = document.querySelector(".client");
+
+let product = document.createElement("div");
+
+product.className = "product";
+
+product.innerHTML = `
+
+<h3>Товар ${productCount}</h3>
+
+<div class="field">
+<label>Название товара</label>
+<input type="text" placeholder="Название">
+</div>
+
+<div class="field">
+<label>Количество</label>
+<input type="number" placeholder="Количество">
+</div>
+
+<div class="field">
+<label>Сумма товара</label>
+<input type="number" placeholder="Сумма">
+</div>
+
+`;
+
+client.insertBefore(product, client.querySelector("button"));
+
+}
